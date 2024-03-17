c189c8201493042e12d51308713893186bc2a510
phadhiil001 <phadhiil001@gmail.com>
2024-03-17 15:27:18 -0500

    Sign-In feature
    This commit was done after the signin feature was created.

---
+++ signin-logic.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+require('config/db_connect.php');
+
+if (isset($_POST['submit'])) {
+    //  Get the data from form
+    $username_email = filter_input(INPUT_POST, 'username_email', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    if (!$username_email) {
+        $_SESSION['error'] = "Username or Email required";
+        $_SESSION['save'] = $_POST; // Save form data to session
+        header('Location: signin.php');
+        exit();
+    } elseif (!$password) {
+        $_SESSION['error'] = "Password is required.";
+        $_SESSION['save'] = $_POST;
+        header('Location: signin.php?err=pass');
+    } else {
+        // Check user credentials in database
+        $query_check = "SELECT * FROM artcityusers  WHERE username= :username_email OR email= :username_email";
+        $stmt = $db->prepare($query_check);
+
+        // Bind parameters
+        $stmt->bindParam(':username_email', $username_email);
+
+        // Execute the query
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user) {
+            // User found, verify password
+            if (password_verify($password, $user['password'])) {
+                // Password is correct
+                $_SESSION['user_id'] = $user['id'];
+                // Set session if user is an admin 
+                if ($user['is_admin'] == 1) {
+                    $_SESSION['user_is_admin'] = true;
+                }
+                $_SESSION['username'] = $user['username'];
+                header('Location: dashboard.php');
+                exit();
+            } else {
+                $_SESSION['error'] = "Invalid password";
+                $_SESSION['save'] = $_POST;
+                header('Location: signin.php');
+                exit();
+            }
+        } else {
+            $_SESSION['error'] = "User not found";
+            $_SESSION['save'] = $_POST;
+            header('Location: signin.php');
+            exit();
+        }
+    }
+} else {
+    header('Location: signin.php');
+    exit();
+}
--- signin.php
+++ signin.php
@@ -3,6 +3,13 @@
 session_start();
 require('config/db_connect.php');
 
+// Retrieve saved values from session
+$username_email = $_SESSION['save']['username_email'] ?? null;
+$password = $_SESSION['save']['password'] ?? null;
+
+// Unset the saved values from session to prevent them from persisting after use
+unset($_SESSION['save']);
+
 ?>
 
 <!DOCTYPE html>
@@ -18,24 +25,26 @@ require('config/db_connect.php');
     <section>
         <div>
             <h2>Sign In</h2>
-            <?php if (isset($_SESSION['error'])) : ?>
+            <?php if (isset($_SESSION['registration'])) : ?>
+                <div class="error-message">
+                    <p><?= $_SESSION['registration']; ?></p>
+                </div>
+                <?php unset($_SESSION['registration']); ?>
+
+            <?php elseif (isset($_SESSION['error'])) : ?>
                 <div class="error-message">
                     <p><?= $_SESSION['error']; ?></p>
                 </div>
                 <?php unset($_SESSION['error']); ?>
             <?php endif ?>
-            
-            <form action="" enctype="multipart/form-data">
-                <input type="text" id="userName" placeholder="User Name">
-                <span class="error-message">
-                    <!-- <p>Please enter your username</p> -->
-                </span>
-                <input type="password" id="enterPassword" placeholder="Enter Password">
-                <span class="error-message">
-                    <!-- <p>Incorrect Password</p> -->
-                </span>
 
-                <button type="submit">Sign In</button>
+            <form action="signin-logic.php" method="post">
+                <input type="text" id="userName" name="username_email" value="<?= htmlspecialchars($username_email) ?>" placeholder="User Name or Email">
+
+                <input type="password" id="enterPassword" name="password" value="<?= htmlspecialchars($password) ?>" placeholder="Enter Password">
+
+
+                <button type="submit" name="submit">Sign In</button>
                 <small>Don't have an account? <a href="signup.php">Sign Up</a></small>
             </form>
         </div>
