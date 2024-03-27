04461014ed9652a63735366bd22db23b72c0ecff
phadhiil001 <phadhiil001@gmail.com>
2024-03-27 10:53:59 -0500

    Home page
    Home page was created and all posts from database can be seen on the home page

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <?php
-session_start();       // Start a new session
-require('connect.php');  // Connect to the database
+session_start();
+require('connect.php');
 
-// Fetch user from the database if logged in
-if (isset($_SESSION['user_id'])) {
-    // Redirect to the dashboard or home page if the user is already logged in
-    header('Location: dashboard.php');
-    exit;
+
+// Fetch featured post including its thumbnail
+$query = "SELECT * FROM artcityposts WHERE is_featured = 1 LIMIT 1";
+$result = $db->query($query);
+
+// Fetch 9 regular posts from the database
+$query_regular = 'SELECT * FROM artcityposts WHERE is_featured = 0 ORDER BY created_date DESC LIMIT 9';
+$regular_posts =  $db->query($query_regular)->fetchAll(PDO::FETCH_ASSOC);
+
+// Check for featured post
+if ($result && $result->rowCount() > 0) {
+    $featured_post = $result->fetch(PDO::FETCH_ASSOC);
+    $featured_post_thumbnail = $featured_post['thumbnail'];
 }
-?>
 
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,7 +30,114 @@ if (isset($_SESSION['user_id'])) {
 <body>
 
     <?php include('nav.php'); ?>
+    <section class="featured">
+        <div class="container featured_container">
+            <?php if (isset($featured_post)) : ?>
+                <div class="post_thumbnail">
+                    <img src="./uploads/<?= $featured_post_thumbnail ?>" alt="Featured Post Image" />
+                </div>
+                <div class="post_info">
+                    <!-- Fetch category title -->
+                    <?php
+                    $category_id = $featured_post["category_id"];
+                    $cat_query = 'SELECT * FROM artcitycategories WHERE id = ?';
+                    $cat_stmt = $db->prepare($cat_query);
+                    $cat_stmt->execute([$category_id]);
+                    $category = $cat_stmt->fetch(PDO::FETCH_ASSOC);
+                    $category_title =  $category['title'];
+                    ?>
+                    <a href="category-post.php?id=<?= $category_title ?>"><?= $category_title ?></a>
+                    <a href="post.php?id=<?= $featured_post['id'] ?>">
+                        <h2 class="post_title"><?= $featured_post['title'] ?></h2>
+                    </a>
+                    <p class="post_content"><?= substr($featured_post['content'], 0, 200) . '...' ?></p>
+                </div>
+                <div class="post_author">
+                    <?php
+                    // Fetch author information
+                    $author_id = $featured_post['author_id'];
+                    $authors_query = "SELECT * FROM artcityusers WHERE id = :author_id";
+                    $authors_stmt = $db->prepare($authors_query);
+                    $authors_stmt->bindParam(":author_id", $author_id, PDO::PARAM_INT);
+                    $authors_stmt->execute();
+                    $author = $authors_stmt->fetch(PDO::FETCH_ASSOC);
+                    ?>
+                    <div class="post_author_img">
+                        <!-- Display user icon -->
+                        <img src="" alt="" />
+                    </div>
+                    <div class="post_author_info">
+                        <h5>By: <?= "{$author['firstname']} {$author['lastname']}" ?></h5>
+                        <small><?= date("M d, Y - H:i", strtotime($featured_post['created_date'])) ?></small>
+                    </div>
+                </div>
+            <?php endif; ?>
 
+        </div>
+    </section>
+
+    <section class="posts">
+        <div class="container post_container">
+            <?php foreach ($regular_posts as $post) : ?>
+                <article class="post">
+                    <div class="post_thumbnail">
+                        <img src="./uploads/<?= $post['thumbnail'] ?>" alt="Post Image" />
+                    </div>
+                    <div class="post_info">
+                        <!-- Fetch category title -->
+                        <?php
+                        $category_id = $post["category_id"];
+                        $cat_query = 'SELECT * FROM artcitycategories WHERE id = ?';
+                        $cat_stmt = $db->prepare($cat_query);
+                        $cat_stmt->execute([$category_id]);
+                        $category = $cat_stmt->fetch(PDO::FETCH_ASSOC);
+                        $category_title =  $category['title'];
+                        ?>
+                        <a href="category-post.php?id=<?= $category_title ?>"><?= $category_title ?></a>
+                        <a href="post.php?id=<?= $post['id'] ?>">
+                            <h2 class="post_title"><?= $post['title'] ?></h2>
+                        </a>
+                        <p class="post_content"><?= substr($post['content'], 0, 150) . '...' ?></p>
+                    </div>
+                    <div class="post_author">
+                        <?php
+                        // Fetch author information
+                        $author_id = $post['author_id'];
+                        $authors_query = "SELECT * FROM artcityusers WHERE id = :author_id";
+                        $authors_stmt = $db->prepare($authors_query);
+                        $authors_stmt->bindParam(":author_id", $author_id, PDO::PARAM_INT);
+                        $authors_stmt->execute();
+                        $author = $authors_stmt->fetch(PDO::FETCH_ASSOC);
+                        ?>
+                        <div class="post_author_img">
+                            <!-- Display user icon -->
+                            <img src="" alt="" />
+                        </div>
+                        <div class="post_author_info">
+                            <h5>By: <?= "{$author['firstname']} {$author['lastname']}" ?></h5>
+                            <small><?= date("M d, Y - H:i", strtotime($post['created_date'])) ?></small>
+                        </div>
+                    </div>
+                </article>
+                <br><br>
+                <hr>
+            <?php endforeach; ?>
+        </div>
+    </section>
+
+    <section class="category_buttons">
+        <div class="container category_buttons">
+            <?php
+            $categories_query = "SELECT * FROM artcitycategories ORDER BY title ASC";
+            $categories_stmt = $db->prepare($categories_query);
+            $categories_stmt->execute();
+            ?>
+
+            <?php while ($category = $categories_stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+                <a href="category-post.php?id=<?= $category['id'] ?>"><?= $category['title'] ?></a>
+            <?php endwhile; ?>
+        </div>
+    </section>
 
 </body>
 
